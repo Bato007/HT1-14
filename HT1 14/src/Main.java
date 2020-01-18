9a876a614bf1a407716ec3a8ec97dcd0ae4f8754
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-01-17 22:17:03 -0600

    Ya hice la interfaz, puse los metodos en el radio e instancie en el main

--- HT1 14/src/Main.java
+++ HT1 14/src/Main.java
@@ -1,4 +1,13 @@
-
+/* Autores:
+ * Pablo
+ * Brandon Hernández - 19376
+ * Fecha de Creación: 17 de Enero de 2020
+ * Nombre: Main.java 
+ * 
+ * Vista del un radio, se encarga de arrancar el programa.
+ */
 public class Main {
-
+	public static void main(String[] args) {
+		Radio miRadio = new Radio();
+	}
 }
--- HT1 14/src/RadioInterface.java
+++ HT1 14/src/RadioInterface.java
@@ -1,4 +1,24 @@
-
+/* Autores:
+ * Pablo
+ * Brandon Hernández - 19376
+ * Fecha de Creación: 17 de Enero de 2020
+ * Nombre: RadioInterface.java 
+ * 
+ * Modelo de un radio.
+ */
 public interface RadioInterface {
 
+	public String estacionActual();
+	
+	public Boolean estado();
+	
+	public void onOff();
+	
+	public void cambiarFrecuencia();
+	
+	public void avanzar();
+	
+	public void guardar(int boton);
+	
+	public void seleccionarEmisora(int boton);
 }
